cfcfe6f2cc808762d9b8f136ed234e9dd0a6fef7
Kodedly <meetenebelioluchi@gmail.com>
2019-02-12 11:38:18 +0100

    error handling for solidity and web3

--- controllers/test.js
+++ controllers/test.js
@@ -54,13 +54,13 @@ module.exports = {
       //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f"
       // );
 
-      const result3 = await token.updateShareholder(
-        "c32214f0887908a8607c9db7d79b87531ae939a40056c3a7858f532d3f8408de",
-        "0xbb723b459f84c24665a89159d94701321864e5d0",
-        "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
-        true,
-        false
-      );
+      // const result3 = await token.updateShareholder(
+      //   "c32214f0887908a8607c9db7d79b87531ae939a40056c3a7858f532d3f8408de",
+      //   "0xbb723b459f84c24665a89159d94701321864e5d0",
+      //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
+      //   true,
+      //   false
+      // );
       // const result4 = await token.isValidShareholder(
       //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
       //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f"
@@ -84,10 +84,10 @@ module.exports = {
       //   "Upfront Schedule",
       //   "This is an upfront for salary advance to Uche"
       // );
-      const result2 = await token.getSchedule(
-        "0xbb723b459f84c24665a89159d94701321864e5d0",
-        1
-      );
+      // const result2 = await token.getSchedule(
+      //   "0xbb723b459f84c24665a89159d94701321864e5d0",
+      //   1
+      // );
       // const result3 = await token.approveSchedule(
       //   "3c10300074f325d4ccd979a21a494a7d9042371b707dda64320817375b160097",
       //   "0x45d2387c6c99c49b859feffbc029d7e605106298",
@@ -106,16 +106,16 @@ module.exports = {
       //   1,
       //   "This is an upfront for salary advance to Uche"
       // );
-      const result6 = await token.mint(
-        "c32214f0887908a8607c9db7d79b87531ae939a40056c3a7858f532d3f8408de",
-        "0xbb723b459f84c24665a89159d94701321864e5d0",
-        1,
-        "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
-        10,
-        "Tradable",
-        0,
-        "This is an upfront minting to Uche"
-      );
+      // const result6 = await token.mint(
+      //   "c32214f0887908a8607c9db7d79b87531ae939a40056c3a7858f532d3f8408de",
+      //   "0xbb723b459f84c24665a89159d94701321864e5d0",
+      //   1,
+      //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
+      //   10,
+      //   "Tradable",
+      //   0,
+      //   "This is an upfront minting to Uche"
+      // );
       // const result7 = await token.withdraw(
       //   "c32214f0887908a8607c9db7d79b87531ae939a40056c3a7858f532d3f8408de",
       //   "0xbb723b459f84c24665a89159d94701321864e5d0",
@@ -125,32 +125,32 @@ module.exports = {
       //   0,
       //   "Allocation was over"
       // );
-      return res.send({ result2, result3, result6});
       // ================================= For Transfers ================================
-      // const result2 = await token.transfer(
+      // const result1 = await token.transfer(
       //   "3065a5faf500affedf57a744baf7a3a9f567639a20253a97c47d035a15e7e1db",
       //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
-      //   "0x87741ffaf59aa62fb42e26fba4d25daffbf2987f",
+      //   "0x56ad8d441dc225a6c2a4fdd408f8084a378a4ac6",
       //   5
       // );
-      // const result1 = await token.approveSender(
+      // const result2 = await token.approveSender(
       //   "3065a5faf500affedf57a744baf7a3a9f567639a20253a97c47d035a15e7e1db",
       //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
-      //   "0x87741ffaf59aa62fb42e26fba4d25daffbf2987f",
-      //   10
+      //   "0x56ad8d441dc225a6c2a4fdd408f8084a378a4ac6",
+      //   3
       // );
-      // const result2 = await token.getAllowance(
+      // const result3 = await token.getAllowance(
       //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
       //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
-      //   "0x87741ffaf59aa62fb42e26fba4d25daffbf2987f"
+      //   "0x56ad8d441dc225a6c2a4fdd408f8084a378a4ac6"
       // );
-      // const result = await token.transferFrom(
-      //   "825b825f2b3ad4f258ef195b605d9cda5e0f974b4187fe4371ec9b37ae3f5973",
-      //   "0x87741ffaf59aa62fb42e26fba4d25daffbf2987f",
-      //   "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
-      //   "0xbb723b459f84c24665a89159d94701321864e5d0",
-      //   5
-      // );
+      const result4 = await token.transferFrom(
+        "82dbdb6ab9df12473e066c124e415d1f0aadfe4f15230677be7af402849e84c0",
+        "0x56ad8d441dc225a6c2a4fdd408f8084a378a4ac6",
+        "0x1F08822Bb986329241Ba2Cfe4209167506483C4f",
+        "0x2f25bbd78503765ba61d83f657e5c3c1f54456ba",
+        0
+      );
+      return res.send({ result4});
       // ====================== For Error Messages ==========================
       // const result1 = await token.addErrorMessage(
       //   "c32214f0887908a8607c9db7d79b87531ae939a40056c3a7858f532d3f8408de",
