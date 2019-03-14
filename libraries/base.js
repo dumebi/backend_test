2947d00964ad995e551a88486e8d56d9f2f566db
Oluchi Enebeli <Oluchi.Enebeli@Sterling.ng>
2019-03-14 13:31:08 +0100

    resolved conflict

--- libraries/base.js
+++ libraries/base.js
@@ -11,43 +11,42 @@ let coinbase = '';
 const provider = utils.config.blockchain;
 const wsProvider = new Web3.providers.WebsocketProvider("ws://localhost:7545");
 const web3 = new Web3(wsProvider);
-// const ganache = require("ganache-cli");
-// const server = ganache.server();
-// server.listen(7545, function(err, blockchain) {
-//   console.log("blockchain >> ", blockchain);
-// });
+const ganache = require("ganache-cli");
+const server = ganache.server();
+server.listen(7545, function(err, blockchain) {
+});
 
-// /**
-//  * Get Coinbase address and amount
-//  */
-// exports.getCoinbase = async () => {
-//   try {
-//     // console.log('coinbase')
-//     web3.eth.getCoinbase((err, account) => {
-//       if (err === null) {
-//         // console.log(err, coinbase)
-//         coinbase = account
-//         // $('#account').text(account);
-//         web3.eth.getBalance(account, (error, balance) => {
-//           if (error === null) {
-//             console.log(error, balance)
-//             coinbase_amount = web3.utils.fromWei(balance, 'ether')
-//             console.log(coinbase, coinbase_amount)
-//           }
-//         })
-//       } else {
-//         console.log(err)
-//       }
-//     })
-//   } catch (err) {
-//     console.log('error')
-//     console.log(err)
-//   }
-// };
+/**
+ * Get Coinbase address and amount
+ */
+exports.getCoinbase = async () => {
+  try {
+    // console.log('coinbase')
+    web3.eth.getCoinbase((err, account) => {
+      if (err === null) {
+        // console.log(err, coinbase)
+        coinbase = account
+        // $('#account').text(account);
+        web3.eth.getBalance(account, (error, balance) => {
+          if (error === null) {
+            console.log(error, balance)
+            coinbase_amount = web3.utils.fromWei(balance, 'ether')
+            console.log(coinbase, coinbase_amount)
+          }
+        })
+      } else {
+        console.log(err)
+      }
+    })
+  } catch (err) {
+    console.log('error')
+    console.log(err)
+  }
+};
 
-// exports.web3 = web3;
-// // exports.UserContract = UserContract
-// exports.coinbase = coinbase
-// exports.coinbase_amount = coinbase_amount
-// exports.ethUtil = ethUtil
-// exports.EthereumTx = EthereumTx
+exports.web3 = web3;
+// exports.UserContract = UserContract
+exports.coinbase = coinbase
+exports.coinbase_amount = coinbase_amount
+exports.ethUtil = ethUtil
+exports.EthereumTx = EthereumTx
