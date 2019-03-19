bf4b0964216bb19de181b78f3098980f80dada83
Jude Dike <dikejude49@gmail.com>
2019-03-19 20:52:19 +0100

    redis

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,15 +1,15 @@
 
 const redis = require('redis');
-const {promisify} = require('util');
+
 const client = redis.createClient(process.env.REDIS_URL);
 
 client.on('connect', () => {
   console.log('connected to redis server');
 })
 
-module.exports = {
-  ...client,
-  getAsync: promisify(client.get).bind(client),
-  setAsync: promisify(client.set).bind(client),
-  keysAsync: promisify(client.keys).bind(client)
-};
+const { promisify } = require('util');
+
+const getAsync = promisify(client.get).bind(client);
+
+exports.getAsync = getAsync;
+exports.client = client;
