feaf8c61847717d8b7ba4a9fc3d00c6f5eff65c2
Faruq Alli-Balogun <faruq-alli-balogun@Faruqs-MacBook-Pro.local>
2019-04-02 06:22:50 +0100

    Created schedule route

--- routes/index.js
+++ routes/index.js
@@ -93,12 +93,14 @@ router.patch('/users/:id/type', middleware.isAdmin, UserController.changeType);
 router.patch('/users/:id/group', middleware.isAdmin, UserController.changeGroup);
 router.patch('/users/:id/employment-status', middleware.isAdmin, UserController.changeEmployment);
 
-router.get('/admin/schedule/', middleware.isAdmin, ScheduleController.all);
 router.post('/admin/schedule/', middleware.isAdmin, ScheduleController.create);
-router.get('/admin/schedule/:schedule_id', middleware.isAdmin, ScheduleController.one);
 router.patch('/admin/schedule/:schedule_id', middleware.isAdmin, ScheduleController.update);
-router.patch('/admin/schedule/enable/:schedule_id', middleware.isAdmin, ScheduleController.enable);
-router.patch('/admin/schedule/disable/:schedule_id', middleware.isAdmin, ScheduleController.enable);
+router.get('/admin/schedule/:schedule_id', middleware.isAdmin, ScheduleController.one);
+router.delete('admin/schedule/:schedule_id', middleware.isAdmin, ScheduleController.delete)
+router.get('/admin/schedule/', middleware.isAdmin, ScheduleController.all);
+
+// router.patch('/admin/schedule/enable/:schedule_id', middleware.isAdmin, ScheduleController.enable);
+// router.patch('/admin/schedule/disable/:schedule_id', middleware.isAdmin, ScheduleController.enable);
 
 router.get('/admin/dividend/', middleware.isAdmin, DividendController.all);
 router.post('/admin/dividend/', middleware.isAdmin, DividendController.create);
