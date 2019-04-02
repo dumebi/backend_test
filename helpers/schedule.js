c76f2275176aea370c6f8236275cabaac80130e8
Oluchi Enebeli <Oluchi.Enebeli@Sterling.ng>
2019-04-02 12:13:18 +0100

    edited the schedule publisher

--- helpers/schedule.js
+++ helpers/schedule.js
@@ -9,36 +9,31 @@ const secure = require('../helpers/encryption.js');
    */
   const initializeToken = async (userId) => {
     try {
+      console.log("userId >> ", userId)
       const user = await UserModel.findById(userId).select('+privateKey')
-      
+      console.log("user >> ",user)
       const privateKey = await secure.decrypt(user.privateKey)
       const sit = new Token('0x'+privateKey);
-      
-      return {
-          SIT : sit
-      }
+      console.log("sit >> ", sit)
+      return sit
   
     } catch (err) {
       console.log("err >> ", err)
-      return res.status(HttpStatus.SERVER_ERROR).json({
-        status: 'failed',
-        data: 'Server error!.'
-      })
+      throw err
     }
   }
 
-exports.create_schedule_on_blockchain = (userId, scheduleId, amount, scheduleType, reason) => {
-    // console.log('First Thingy => ', scheduleId, amount, scheduleType, reason);
-    (async function (req, res, next) {
-        "use strict";
-
+exports.create_schedule_on_blockchain = async (userId, scheduleId, amount, scheduleType, reason) => {
+    // (async function () {
+    //     "use strict";
         try {
-            const result = await initializeToken(userId).SIT.createSchedule(scheduleId, amount, scheduleType, reason);
+          console.log("userId2 >> ", userId)
+          const SIT = await initializeToken(userId)
+            const result = await SIT.createSchedule(scheduleId, amount, scheduleType, reason);
             console.log('Result => ', result);
 
         } catch (error) {
             console.log('Second Thingy => ', error);
-
         }
-    }());
+    // }());
 }
